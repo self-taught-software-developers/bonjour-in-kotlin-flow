334d69639405a1a3e7a4f8f1b88625b39e25db59
Revok <cerve.v55@gmail.com>
2022-09-28 17:03:15 -0400

    reformatting packaging.

--- sample/src/main/java/com/cerve/co/sample/di/Module.kt
+++ sample/src/main/java/com/cerve/co/sample/di/Module.kt
@@ -1,21 +1,20 @@
 package com.cerve.co.sample.di
 
 import android.content.Context
-
+import com.cerve.co.bonjour_in_flow.TimedBonjourInFlow
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Inject
 import javax.inject.Singleton
 
 @[dagger.Module InstallIn(SingletonComponent::class)]
 object Module {
 
-//    @Singleton
-//    @Provides
-//    fun providesTimedBonjourInFlow(
-//        @ApplicationContext context: Context
-//    ) = TimedBonjourInFlow(context)
+    @Singleton
+    @Provides
+    fun providesTimedBonjourInFlow(
+        @ApplicationContext context: Context
+    ) = TimedBonjourInFlow(context)
 
 }
--- sample/src/main/java/com/cerve/co/sample/di/SampleApplication.kt
+++ sample/src/main/java/com/cerve/co/sample/di/SampleApplication.kt
@@ -1,4 +1,4 @@
-package com.cerve.co.sample
+package com.cerve.co.sample.di
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
