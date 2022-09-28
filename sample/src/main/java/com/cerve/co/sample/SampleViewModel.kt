d08b451bcf31d48350935d63a763f3f4330ed37e
Revok <cerve.v55@gmail.com>
2022-09-28 17:09:29 -0400

    sample app wiring with remote jitpack reference complete.

--- sample/src/main/java/com/cerve/co/sample/SampleViewModel.kt
+++ sample/src/main/java/com/cerve/co/sample/SampleViewModel.kt
@@ -3,16 +3,19 @@ package com.cerve.co.sample
 import android.net.nsd.NsdServiceInfo
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.cerve.co.bonjour_in_flow.BonjourInFlow
+import com.cerve.co.bonjour_in_flow.discover.DiscoverEvent
 
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SampleViewModel @Inject constructor(
-//    timedBonjourInFlow: TimedBonjourInFlow
+    private val bonjourInFlow: BonjourInFlow
 ) : ViewModel() {
 
     private val _sampleUi = MutableStateFlow(SampleUiState())
@@ -21,29 +24,23 @@ class SampleViewModel @Inject constructor(
     init {
         viewModelScope.launch {
 
-//            bonjourInFlow.discoverServicesWithState()
-//                .collect { event ->
-//
-//                    when(event) {
-//                        is DiscoverEvent.ServiceFound,
-//                        is DiscoverEvent.ServiceUnResolved,
-//                        is DiscoverEvent.ServiceResolved -> _sampleUi.update {
-//                            it.copy(nsdItems = it.add(event.serviceInfo()))
-//                        }
-//                        is DiscoverEvent.ServiceLost -> _sampleUi.update {
-//                            it.copy(nsdItems = it.remove(event.service))
-//                        }
-//                        is DiscoverEvent.DiscoveryStarted -> _sampleUi.update { it.copy(nsdState = UiDiscoveryState.DISCOVERING) }
-//                        else -> _sampleUi.update { it.copy(nsdState = UiDiscoveryState.IDLE) }
-//                    }
-//
-////                }
-//            timedBonjourInFlow.searchByNameWithTimeout(
-//                name = "SIMPLEconnect Fan-C6985E",
-//                types = Pair(REALTEK_TYPE, MARVELL_TYPE)
-//            )?.let {
-//
-//            }
+            bonjourInFlow.discoverServicesWithState()
+                .collect { event ->
+
+                    when(event) {
+                        is DiscoverEvent.ServiceFound,
+                        is DiscoverEvent.ServiceUnResolved,
+                        is DiscoverEvent.ServiceResolved -> _sampleUi.update {
+                            it.copy(nsdItems = it.add(event.serviceInfo()))
+                        }
+                        is DiscoverEvent.ServiceLost -> _sampleUi.update {
+                            it.copy(nsdItems = it.remove(event.service))
+                        }
+                        is DiscoverEvent.DiscoveryStarted -> _sampleUi.update { it.copy(nsdState = UiDiscoveryState.DISCOVERING) }
+                        else -> _sampleUi.update { it.copy(nsdState = UiDiscoveryState.IDLE) }
+                    }
+
+                }
 
         }
     }
--- sample/src/main/java/com/cerve/co/sample/di/Module.kt
+++ sample/src/main/java/com/cerve/co/sample/di/Module.kt
@@ -1,7 +1,7 @@
 package com.cerve.co.sample.di
 
 import android.content.Context
-import com.cerve.co.bonjour_in_flow.TimedBonjourInFlow
+import com.cerve.co.bonjour_in_flow.BonjourInFlow
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -15,6 +15,6 @@ object Module {
     @Provides
     fun providesTimedBonjourInFlow(
         @ApplicationContext context: Context
-    ) = TimedBonjourInFlow(context)
+    ) = BonjourInFlow(context)
 
 }
